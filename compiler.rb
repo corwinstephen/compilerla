70ac7d817465521a333988c5fff319f7e1d144f7
Stephen Corwin <corwinstephen@gmail.com>
2014-12-03 13:57:58 -0800

    Got a homepage running

--- compiler.rb
+++ compiler.rb
@@ -1,7 +1,24 @@
 require 'rubygems'
 require 'sinatra'
+require 'sinatra/base'
+require 'sinatra/assetpack'
 require 'haml'
 
 get '/' do
   haml :index
+end
+
+assets do
+
+  js :application, [
+    '/js/*.js'
+  ]
+
+  css :application, [
+    '/css/*.css',
+  ]
+
+  js_compression :jsmin
+  css_compression :scss
+
 end
