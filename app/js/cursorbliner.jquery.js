5858116fe99ca81dbe45b038b1036b1174d5f90f
Stephen Corwin <corwinstephen@gmail.com>
2014-12-03 18:34:35 -0800

    Top links

--- app/js/cursorbliner.jquery.js
+++ app/js/cursorbliner.jquery.js
@@ -1,6 +1,9 @@
 $.fn.textWidth = function(text, font) {
-    if (!$.fn.textWidth.fakeEl) $.fn.textWidth.fakeEl = $('<span>').hide().appendTo(document.body);
-    $.fn.textWidth.fakeEl.text(text || this.val() || this.text()).css('font', font || this.css('font'));
+    if (!$.fn.textWidth.fakeEl) $.fn.textWidth.fakeEl = $('<span style="display:inline;">').appendTo(document.body);
+    var htmlText = text || this.val() || this.text();
+    htmlText = $.fn.textWidth.fakeEl.text(htmlText).html(); //encode to Html
+    htmlText = htmlText.replace(/\s/g, "&nbsp;"); //replace trailing and leading spaces
+    $.fn.textWidth.fakeEl.html(htmlText).css('font', font || this.css('font'));
     return $.fn.textWidth.fakeEl.width();
 };
 
