9667dbe2c1df4b91f221646c93a2adc695e2523a
Stephen Corwin <corwinstephen@gmail.com>
2014-12-07 21:37:53 -0800

    Connected homepage to NationBuilder

--- compiler.rb
+++ compiler.rb
@@ -3,6 +3,8 @@ require 'sinatra/base'
 require 'sinatra/assetpack'
 require 'haml'
 require 'sass'
+require 'nationbuilder'
+require 'json'
 
 class Compiler < Sinatra::Base
 
@@ -25,6 +27,20 @@ class Compiler < Sinatra::Base
 
   get '/' do
     haml :index
+  end
+
+  post '/people' do
+    content_type :json
+
+    nb_client = NationBuilder::Client.new('dtla', '8fe18b901096775f283bb9bfd0ec70ca24689feb4dcd3c5262f1a960fb87f246')
+    post_data = {
+      person: {
+        email: params[:email],
+        tags: ["compiler", "compilerla website signup"]
+      }
+    }
+
+    nb_client.call(:people, :create, post_data).to_json
   end
 
 end
